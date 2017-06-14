c3dd609f2d870eb0c028c2941eb1d1aca8044870
kpescobar <kellyp.721@live.com>
2017-06-14 08:47:01 -0700

    Added try, catch, and err message.

--- src/Gcd.java
+++ src/Gcd.java
@@ -2,11 +2,16 @@
 public class Gcd {
 
   public static void main(String[] args) {
-    int a = Integer.parseInt(args[0]);
-    int b = Integer.parseInt(args[1]);
-    System.out.printf("a= %d, b= %d%n", a, b);
+    try {
+      int a = Integer.parseInt(args[0]);
+      int b = Integer.parseInt(args[1]);
+      System.out.printf("a= %d, b= %d%n", a, b);
+    } catch (NumberFormatException ex) {
+      ex.printStackTrace();
+      System.out.println("This application requires two integer arguments on the command line.");
+    }
   }
-  
-  
+
+
 
 }
