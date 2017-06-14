411d902168c9b0eac4789509ebf2ce5657a0c932
kpescobar <kellyp.721@live.com>
2017-06-14 08:22:05 -0700

    Establishing variables.

---
+++ src/Gcd.java
@@ -0,0 +1,12 @@
+
+public class Gcd {
+
+  public static void main(String[] args) {
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    System.out.printf("a= %d, b= %d%n", a, b);
+  }
+  
+  
+
+}
